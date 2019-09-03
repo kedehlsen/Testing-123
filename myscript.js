d5dc21c2b5c389865a8f858a5176300b5fcd66f9
kedehlsen <kedehlsen@gmail.com>
2019-09-03 10:01:05 -0700

    javascript

--- myscript.js
+++ myscript.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
 
-	$("p").click(function(){
-		$("p").hide();
+	$("#hide").click(function(){
+		$("#hide").hide();
 	});
+	
+	$("img").click(function(){
+		$("p").show();
+	});
+	
+	$(".stl").css(
+	{ "color":"red"});
 
 });
